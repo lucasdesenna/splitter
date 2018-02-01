8aba3a7c5a0ea58934a45b081b6ceae3645036de
Lucas de Senna <lucasdesenna@gmail.com>
2018-02-01 00:18:12 -0200

    FEAT-Improve EntryList display order

--- src/features/EntryListView/EntryList.js
+++ src/features/EntryListView/EntryList.js
@@ -1,5 +1,6 @@
 // @flow
 import React from 'react';
+import moment from 'moment';
 
 import { List } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
@@ -12,17 +13,66 @@ type Props = {
   entries: EntryType[],
 };
 
+const groupBy = (entrylist: EntryType[], dateFormat) => {
+  const grouped = [];
+  const temp = entrylist.reduce((groupedList, entry) => {
+    const groupValue = moment(entry.isoTimestamp).format(dateFormat);
+
+    (groupedList[groupValue] = groupedList[groupValue] || []).push(entry);
+
+    return groupedList;
+  }, {});
+
+  for (let group in temp) {
+    grouped.push({
+      name: group,
+      value: temp[group],
+    });
+  }
+
+  return grouped;
+};
+
+const groupByYearAndMonth = (entrylist: EntryType[]): any[] => {
+  const groupedByYearAndMonth = groupBy(entrylist, 'YYYY').map(year => {
+    year.value = groupBy(year.value, 'MMMM');
+
+    return year;
+  });
+
+  return groupedByYearAndMonth;
+};
+
 const sortByTimestamp = (a: EntryType, b: EntryType): number =>
   new Date(a.isoTimestamp).getTime() - new Date(b.isoTimestamp).getTime();
 
+const entriesByYearAndMont = entryList => {
+  const groupedByYearAndMonth = groupByYearAndMonth(
+    entryList.sort(sortByTimestamp)
+  );
+  const entriesByYearAndMonth = [];
+
+  groupedByYearAndMonth.reverse().map(year => {
+    year.value.reverse().map(month => {
+      entriesByYearAndMonth.push(
+        <div key={`entryGroup-${year.name}-${month.name}`}>
+          <Subheader>{`${month.name} ${year.name}`}</Subheader>
+          {month.value
+            .reverse()
+            .map((entry, index) => (
+              <Entry key={`entry-${month.name}-${index}`} entryData={entry} />
+            ))}
+        </div>
+      );
+    });
+  });
+
+  return entriesByYearAndMonth;
+};
+
 const entryList = (props: Props) => (
   <List className={styles.EntryList}>
-    <Subheader>Today</Subheader>
-    {props.entries
-      .sort(sortByTimestamp)
-      .map((entry, index) => (
-        <Entry key={`entry-${index}`} entryData={entry} />
-      ))}
+    {entriesByYearAndMont(props.entries)}
   </List>
 );
 
